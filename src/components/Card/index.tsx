eacbf4030119679fb4fcb5324fdc85955584e1d6
ropoko <rodrigostramantinoli@gmail.com>
2022-05-30 11:21:59 -0300

    feat: show character details

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Character } from '../../store/features/characters/types';
 import './card.style.css';
 
@@ -6,7 +7,9 @@ const Card = ({ char }: { char: Character }) => {
 	return (
 		<div className='card'>
 			<header className='card-img'>
-				<img className='char-picture' src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt={char.name} />
+				<Link to={`character/${char.id}`}>
+					<img className='char-picture' src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt={char.name} />
+				</Link>
 			</header>
 			<main className='card-content'>
 				<strong> {char.name} </strong>
